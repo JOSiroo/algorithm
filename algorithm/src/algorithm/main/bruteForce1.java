758a3cf2e1acde379bf48ee7f3d13c6cff85985a
ezen <ezen@DESKTOP-O03MT84>
2023-06-29 12:36:07 +0900

    230629 난쟁이문제(브루트포스)

--- algorithm/src/algorithm/main/bruteForce1.java
+++ algorithm/src/algorithm/main/bruteForce1.java
@@ -5,10 +5,38 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class bruteForce1 {
-	
-	
+	static int sum;
+	static int[] hobit;
+
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		hobit = new int[9];
+		sum = 0;
+		for(int i = 0; i < 9; i++) {
+			hobit[i] = sc.nextInt();
+			sum += hobit[i];
+		}
+		for(int i = 0; i < 9; i++) {
+			for(int k = 0; k < 9; k++) {
+				if(sum - (hobit[i] + hobit[k]) == 100 && i != k) {
+					ArrayList<Integer> list = new ArrayList<>();
+					int num1 = hobit[i];
+					int num2 = hobit[k];
+					for(int a : hobit) {
+						if(a != num1 && a != num2) {
+							list.add(a);
+						}
+					}
+					Collections.sort(list);
+					for(int a : list) {
+
+						System.out.println(a);
+					}
+					return;
+				}
+			}
+
+		}
 	}
 
 }
